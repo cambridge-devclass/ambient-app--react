4e8011b53d6b6ccb998090051a57d51d644d3e38
hattifnatt4r <dolgareva.ma@gmail.com>
2025-08-18 16:03:04 -0400

    More setups

---
+++ server/_migrate.py
@@ -0,0 +1,8 @@
+# Apply migration from migrations/ folder manually
+
+# 1. Get migration file name as argument
+# 2. Read sql from the migration file (migrations/migration_date_name.sql)
+# 3. Read db credentials from db.py
+# 4. Use mysql.connector to connect to the database
+# 5. Apply migration and commit
+
---
+++ server/config/db_example.py
@@ -0,0 +1,6 @@
+dbconfig = {
+  'user': 'dbuser',
+  'password': 'dbpassword',
+  'host': '127.0.0.1',
+  'database': 'devclass_ambient_app'
+}
